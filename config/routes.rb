110a554acaf95be7e253ee04abf0a732ee691cd8
Jeroen Visser <jeroenvisser101@gmail.com>
2015-01-03 19:19:21 +0100

    Styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+             path:       '/',
+             path_names: {
+               sign_in:      'login',
+               sign_out:     'logout',
+               password:     'recovery',
+               confirmation: 'verification',
+               edit:         'profile/edit',
+               registration: 'sign-up'
+             }
 
   root to: 'site#index'
 
