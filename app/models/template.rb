9abb3ab4c4cf617b29f584b9f0b827b16e40deb6
Jeroen Visser <jeroenvisser101@gmail.com>
2015-02-13 21:31:01 +0100

    Added validations to the model.

--- app/models/template.rb
+++ app/models/template.rb
@@ -1,6 +1,9 @@
 class Template < ActiveRecord::Base
   belongs_to :user
 
+  validates :name, presence: true, length: { in: 3..255 }
+  validates :content, presence: true
+
   def render(data = {})
     @template = Liquid::Template.parse(content)
     @template.render(data)
