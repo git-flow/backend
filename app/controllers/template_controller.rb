9bb12bb20992944e4bb2efdb08c773767521ae07
Jeroen Visser <jeroenvisser101@gmail.com>
2015-02-13 21:31:39 +0100

    You can now create templates.

--- app/controllers/template_controller.rb
+++ app/controllers/template_controller.rb
@@ -2,9 +2,17 @@ class TemplateController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:render_template]
 
   respond_to :json
-
   def new
     @template = Template.new
+  end
+
+  respond_to :json, :html
+  def create
+    @template = Template.new(create_template_params)
+    @template.user = current_user
+    @template.save
+
+    respond_with @template
   end
 
   def render_template
@@ -14,7 +22,7 @@ class TemplateController < ApplicationController
     return render status: :not_found unless @template
 
     # Render the template itself
-    output = @template.render(template_params)
+    output = @template.render(render_template_params)
 
     respond_with({ template: { output: output } },
                  location: template_path(params[:id]))
@@ -22,7 +30,11 @@ class TemplateController < ApplicationController
 
   private
 
-  def template_params
+  def render_template_params
     params.require(:template)
+  end
+
+  def create_template_params
+    params.require(:template).permit(:name, :content)
   end
 end
